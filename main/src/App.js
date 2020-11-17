a5cb8335c72789c010052024bef04d404d1933d2
Toni Pekkala <tonipekkala90@gmail.com>
2020-11-17 23:52:46 +0200

    added react

---
+++ main/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  const [info, setInfo] = useState([]);
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ main/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Header;
